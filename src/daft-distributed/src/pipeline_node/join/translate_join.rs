5328a117bd0a1aade243503f6969923f2e066901
Anshul <anshulgoel27@gmail.com>
2026-05-20 10:04:47 +0530

    error fix

--- src/daft-distributed/src/pipeline_node/join/translate_join.rs
+++ src/daft-distributed/src/pipeline_node/join/translate_join.rs
@@ -235,6 +235,7 @@ impl LogicalPlanToPipelineNodeTranslator {
                 )),
                 left.config().schema.clone(),
                 left,
+                left_stats.size_bytes,
             )?
         } else {
             left
@@ -250,6 +251,7 @@ impl LogicalPlanToPipelineNodeTranslator {
                 )),
                 right.config().schema.clone(),
                 right,
+                right_stats.size_bytes,
             )?
         } else {
             right
