e57aa125e7b73ecde66a12aba70dd01033737e82
Anshul <anshulgoel27@gmail.com>
2026-06-04 12:27:26 +0530

    removing flight_shuffle from auto

--- src/common/daft-config/src/lib.rs
+++ src/common/daft-config/src/lib.rs
@@ -202,7 +202,7 @@ impl Default for DaftExecutionConfig {
             enable_dynamic_batching: false,
             dynamic_batching_strategy: "auto".to_string(),
             flight_shuffle_dirs: vec!["/tmp".to_string()],
-            flight_shuffle_compression: Some("lz4".to_string()),
+            flight_shuffle_compression: None,
             flight_shuffle_partition_threshold: 500,
             enable_multi_glob_path_tasks: false,
             hash_join_spill_threshold_bytes: None,
